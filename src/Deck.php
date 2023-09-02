e258caca3eec45b19edce77b7788a2351d241c01
Russell Purinton <russell.purinton@gmail.com>
2023-09-02 01:39:48 -0400

    4

--- src/Deck.php
+++ src/Deck.php
@@ -3,32 +3,6 @@
 namespace RPurinton\poker;
 
 require_once 'Card.php';
-/* for refreence from Card.php:
-enum Suit: string
-{
-    case CLUBS = 'clubs';
-    case DIAMONDS = 'diamonds';
-    case HEARTS = 'hearts';
-    case SPADES = 'spades';
-}
-
-enum Rank: int
-{
-    case TWO = 2;
-    case THREE = 3;
-    case FOUR = 4;
-    case FIVE = 5;
-    case SIX = 6;
-    case SEVEN = 7;
-    case EIGHT = 8;
-    case NINE = 9;
-    case TEN = 10;
-    case JACK = 11;
-    case QUEEN = 12;
-    case KING = 13;
-    case ACE = 14;
-}
-*/
 
 use RPurinton\poker\Card;
 
@@ -62,5 +36,10 @@ class Deck
             $string .= $card . "\n";
         }
         return $string;
+    }
+
+    public function __toArray(): array
+    {
+        return $this->cards;
     }
 }
