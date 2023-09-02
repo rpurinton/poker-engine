cf796f88fbfe43fe5550ae085b760645362c8662
Russell Purinton <russell.purinton@gmail.com>
2023-09-02 02:11:58 -0400

    7

--- src/Pot.php
+++ src/Pot.php
@@ -1,3 +1,36 @@
 <?php
 
 namespace RPurinton\poker;
+
+class Pot
+{
+    public function __construct(private float $amount = 0)
+    {
+        $this->amount = round($this->amount, 2);
+    }
+
+    public function add(float $amount): void
+    {
+        $this->amount += round($amount, 2);
+    }
+
+    public function remove(float $amount): void
+    {
+        $this->amount -= round($amount, 2);
+    }
+
+    public function getAmount(): float
+    {
+        return round($this->amount, 2);
+    }
+
+    public function setAmount(float $amount): void
+    {
+        $this->amount = round($amount, 2);
+    }
+
+    public function __toString(): string
+    {
+        return '$' . number_format($this->amount, 2, '.', ',');
+    }
+}
--- test.php
+++ test.php
@@ -6,4 +6,6 @@ namespace RPurinton\poker;
 require_once('src/Deck.php');
 $deck = new Deck();
 $deck->shuffle();
-print_r($deck->toString());
+
+$card = $deck->dealCard();
+print_r($card);
