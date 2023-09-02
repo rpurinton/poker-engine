24446f57394a8bc4f843eedec1a3267ddb6d48d7
Russell Purinton <russell.purinton@gmail.com>
2023-09-02 19:12:46 -0400

    compiler

--- compile.php
+++ compile.php
@@ -2,5 +2,8 @@
 @unlink(__DIR__ . '/compiled.php');
 $contents = "";
 exec("find * | grep '.php'", $files);
-foreach ($files as $file) $contents .= "File: $file:\n" . file_get_contents($file) . "\n";
+foreach ($files as $file) {
+    if ($file == "compile.php") continue;
+    $contents .= "File: $file:\n" . file_get_contents($file) . "\n";
+}
 file_put_contents(__DIR__ . '/compiled.php', $contents);
--- compiled.php
+++ compiled.php
@@ -1,11 +1,3 @@
-File: compile.php:
-<?php
-@unlink(__DIR__ . '/compiled.php');
-$contents = "";
-exec("find * | grep '.php'", $files);
-foreach ($files as $file) $contents .= "File: $file:\n" . file_get_contents($file) . "\n";
-file_put_contents(__DIR__ . '/compiled.php', $contents);
-
 File: src/TableStatus.php:
 <?php
 
